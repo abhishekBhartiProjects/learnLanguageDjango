2313a602463b23dbd766e7215c787c5456374823
abhishekbharti <abhishek.bharti@testbook.com>
2018-12-02 22:48:33 +0530

    blog done

--- learnLanguageProject/urls.py
+++ learnLanguageProject/urls.py
@@ -19,6 +19,8 @@ from hello.views import myView
 from todo.views import myTodo, addTodo, deleteTodo
 from personal.views import personalIndex, contact
 from blog.views import blog
+from django.views.generic import ListView, DetailView
+from blog.models import Post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +30,10 @@ urlpatterns = [
     path('deleteTodo/<int:todo_id>/', deleteTodo),
     path('', personalIndex),
     path('contact/', contact),
-    path('blog/', blog),
+    # path('blog/', blog),
+    path('blog/', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25],
+                    template_name="blog/blog.html")),
+    path('blog/<slug:pk>/', DetailView.as_view(model=Post, template_name="blog/post.html")),
+
+
 ]
